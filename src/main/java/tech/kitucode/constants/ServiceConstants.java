b243df5551ae6405e88545c7781f32b23c4e3e96
kathukyabrian <bkitunda@meliora.tech>
2025-02-25 11:05:58 +0300

    added functionality to load configs from external resources

---
+++ src/main/java/tech/kitucode/constants/ServiceConstants.java
@@ -0,0 +1,15 @@
+package tech.kitucode.constants;
+
+public class ServiceConstants {
+    public static final String DEFAULT_CONFIG_LOCATION = "/configs/mulika/reporting";
+    public static final String DEFAULT_CONFIG_FILE_NAME = "config.properties";
+
+
+    public static final String APP_CONFIG_KEY = "app";
+    public static final String MODULE_CONFIG_KEY = "module";
+    public static final String REPORT_INTERVAL_CONFIG_KEY = "report-interval";
+    public static final String SERVICE_COUNT_CONFIG_KEY = "service-count";
+    public static final String SERVICE_PREFIX_CONFIG_KEY = "service-prefix";
+    public static final String URL_CONFIG_KEY = "url";
+    public static final String API_KEY_CONFIG_KEY = "api-key";
+}
